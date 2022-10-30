db961c56b726ff6a2ddd6c6031af8b51e9e1626f
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-10-29 22:33:26 -0300

    proximo comit

--- java/script.js
+++ java/script.js
@@ -1,4 +1,13 @@
 
+let menu = document.querySelector('#menu')
+let navbar = document.querySelector('.navbar ul')
+
+menu.addEventListener('click', () =>{
+    navbar.classList.toggle('active')
+    console.log("chegou aqui")
+})
+
+
 let swiper1 = new Swiper(".products-slider",{
     spaceBetween: 20,
     loop: true,
